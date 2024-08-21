c56939670b0a0164d6fa99c776d65b05e3b1dabe
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-08-21 10:46:37 -0400

    add chat

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('connected to the web socket');
+
+  socket.on('new message sent', (newMessage) => {
+    console.log('NEW MESSAGE', newMessage);
+
+    io.emit('new message to relay', newMessage);
+  })
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+
